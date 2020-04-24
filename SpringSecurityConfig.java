13c775054ff53ce6caaa603cbda125fc7e6a3b5a
socrate1900 <siekapetliale@yahoo.fr>
2020-04-23 20:05:36 -0400

    Configuration class
    I Updated the Configuration class

---
+++ SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package Techique.Surge.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("mother").password("mother").roles("ADMIN");
+
+	}
+
+	// security for all API
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+	} // Here is the basic request for Authentification }
+
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+
+}
